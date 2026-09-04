a6a0da5a2bea7b5bd66b46b6e45cd410b75e9671
Nate <101449765+EveryoneEverybody@users.noreply.github.com>
2026-09-03 22:07:49 -0700

    Bump managed assembly version for performance test build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// to COM components.  If you need to access this assembly's types from
+// COM, set ComVisible to true on that type.
 [assembly: ComVisible(false)]
 
 //In order to begin building localizable applications, set
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.9.16.0")]
-[assembly: AssemblyFileVersion("1.9.16.0")]
+[assembly: AssemblyFileVersion("1.9.16")]
