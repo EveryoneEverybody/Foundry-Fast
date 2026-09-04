4c9b734a07acf03e1f5d0dfb8813892387e6278b
Nate <101449765+EveryoneEverybody@users.noreply.github.com>
2026-09-03 22:08:07 -0700

    Set managed assembly version to 1.9.18

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access this assembly's types from
-// COM, set ComVisible to true on that type.
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 //In order to begin building localizable applications, set
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.16.0")]
-[assembly: AssemblyFileVersion("1.9.16")]
+[assembly: AssemblyVersion("1.9.18.0")]
+[assembly: AssemblyFileVersion("1.9.18.0")]
